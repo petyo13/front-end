9aae3539a10bff205a4a166cb7c208cf913634ba
petyo13 <45045277+petyo13@users.noreply.github.com>
2019-05-11 08:49:05 +0300

    Add files via upload

---
+++ precache-manifest.9541408996b65c12f39d6bf635d3a970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ab1a740c0d0ea942768",
+    "url": "/static/js/main.8ed3f7af.chunk.js"
+  },
+  {
+    "revision": "2d83da9fc9db503fabb0",
+    "url": "/static/js/2.9bb6b8dd.chunk.js"
+  },
+  {
+    "revision": "4ab1a740c0d0ea942768",
+    "url": "/static/css/main.b48a6324.chunk.css"
+  },
+  {
+    "revision": "54b49e2a83440fdb4c0cc4470dfa76f8",
+    "url": "/index.html"
+  }
+];
